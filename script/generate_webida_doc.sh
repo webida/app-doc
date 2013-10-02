73189dd6e5508734a1dcc99f484eb93e2fbfc25b
taejun <samsung.com>
2013-10-02 16:31:47 +0900

    remove script url option

--- script/generate_webida_doc.sh
+++ script/generate_webida_doc.sh
@@ -4,11 +4,7 @@
 rm -f src/webida.js
 
 #download webida.js file from server using input parameter
-if [ "$1" -gt "-1" ]; then
-    wget https://$1/webida.js --no-check-certificate --directory-prefix=src
-else
-    wget https://webida.org/webida.js --no-check-certificate --directory-prefix=src
-fi
+wget https://webida.org/webida.js --no-check-certificate --directory-prefix=src
 
 #generate doc
 ./node_modules/jsdoc/jsdoc src/webida.js -d doc -t src/templates/webida/
