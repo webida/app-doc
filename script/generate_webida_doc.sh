c07a2c547171abad3e2dd239e051d5d4c84de34a
taejun <samsung.com>
2013-10-25 14:23:46 +0900

    Modify generate script for ci apply

--- script/generate_webida_doc.sh
+++ script/generate_webida_doc.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 #remove old webida.js
-rm -f src/webida.js
+rm -f src/webida-0.2.js
+rm -f src/fs.js
+rm -f src/file.js
 
 #download webida.js file from server using input parameter
-wget https://webi2da.core1/webida.js --no-check-certificate --directory-prefix=src
+#wget https://web2da.core1/webida-0.2.js --no-check-certificate --directory-prefix=src
 
 #generate doc
-./node_modules/jsdoc/jsdoc src/webida.js -d doc -t src/templates/docstrap/template/ -c src/templates/docstrap/template/webida.conf.json
+./node_modules/jsdoc/jsdoc src/webida-0.2.js src/fs.js src/file.js -d doc -t src/templates/docstrap/template/ -c src/templates/docstrap/template/webida.conf.json
