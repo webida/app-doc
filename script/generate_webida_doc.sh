5f605721e58653df2cb6c850422f6e7e82e02b65
taejun <samsung.com>
2013-10-25 14:28:00 +0900

    modify script source file for remove result directory

--- script/generate_webida_doc.sh
+++ script/generate_webida_doc.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-#remove old webida.js
+#remove old document source files
 rm -f src/webida-0.2.js
 rm -f src/fs.js
 rm -f src/file.js
+
+#remove old result files
+rm -rf doc
 
 #download webida.js file from server using input parameter
 #wget https://web2da.core1/webida-0.2.js --no-check-certificate --directory-prefix=src
